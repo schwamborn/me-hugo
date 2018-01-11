1e76b5797a64f8c71df99484f1e48589b964f5e7
Matthias Schwamborn <schwamborn@uos.de>
2018-01-12 00:13:03 +0100

    extend deployment script

--- deploy.sh
+++ deploy.sh
@@ -22,3 +22,10 @@ git push origin master
 
 # Come back up to the project root.
 cd ..
+
+# Update reference of public submodule to HEAD.
+echo -e "\033[0;32mUpdating reference in root folder...\033[0m"
+git add public
+git commit -m "update public submodule ref"
+git push origin master
+
